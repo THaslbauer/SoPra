5aca1bdbc0c885e62c6cbefbb2df91f129cc797e
Nico <s9nigers@stud.uni-saarland.de>
2014-09-12 15:29:08 +0200

    added JavaDoc to the expressions

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/expression/GreaterOperator.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/expression/GreaterOperator.java
@@ -1,12 +1,9 @@
 package de.unisaarland.cs.st.pirates.group1.sim.logic.expression;
 
-public class GreaterOperator extends Operator{
+public class GreaterOperator extends Operator
+{
 
-	/**
-	 * 
-	 * @param leftval
-	 * @param rightval
-	 */
+	
 	public GreaterOperator(RegisterCall leftval, Primary rightval){
 		
 		super(leftval, rightval);
@@ -14,6 +11,8 @@ public class GreaterOperator extends Operator{
 	}
 	
 	/**
+	 * This method returns if the register in leftval is greater than
+	 * the register in rightval.
 	 * 
 	 */
 	public int evaluate (int[] registers){
