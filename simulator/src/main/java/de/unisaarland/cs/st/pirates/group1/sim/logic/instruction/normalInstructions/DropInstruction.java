11ec8430c9d7b3427d56581fd7de971fd2537497
Thomas Haslbauer <s9thhasl@stud.uni-saarland.de>
2014-09-12 16:17:26 +0200

    Added javadoc for Instructions that aren't ElseInstructions

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/normalInstructions/DropInstruction.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/normalInstructions/DropInstruction.java
@@ -4,8 +4,17 @@ import de.unisaarland.cs.st.pirates.group1.sim.logger.ExtendedLogWriter;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Ship;
 
+/**
+ * An Instruction to drop a treasure
+ * @author thomas
+ *
+ */
 public class DropInstruction extends Instruction {
 
+	/**
+	 * Creates the Instruction
+	 * @param logger
+	 */
 	public DropInstruction(ExtendedLogWriter logger) {
 		// TODO Auto-generated constructor stub
 		super(logger);
--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/normalInstructions/TurnInstruction.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/normalInstructions/TurnInstruction.java
@@ -4,9 +4,19 @@ import de.unisaarland.cs.st.pirates.group1.sim.logger.ExtendedLogWriter;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Ship;
 
+/**
+ * Makes a ship turn.
+ * @author thomas
+ *
+ */
 public class TurnInstruction extends Instruction {
 	private boolean left;
 	
+	/**
+	 * Create the Instruction
+	 * @param logger
+	 * @param left Controls if you have to turn left or right (left == true)
+	 */
 	public TurnInstruction(ExtendedLogWriter logger, boolean left){
 		super(logger);
 		this.left = left;
