d3f2be3ee942fc7e8149fb35bf3819e02adba051
Jens Ulrich Kreber <s9jekreb@stud.uni-saarland.de>
2014-09-12 15:02:28 +0200

    added some ship methods, added two enums
    hastag 420yoloswagpattern

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/Ship.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/Ship.java
@@ -1,6 +1,7 @@
 package de.unisaarland.cs.st.pirates.group1.sim.gamestuff;
 
 import de.unisaarland.cs.st.pirates.group1.sim.util.Heading;
+import de.unisaarland.cs.st.pirates.group1.sim.util.Register;
 
 /**
  * The representation of a pirate ship
@@ -31,6 +32,127 @@ public class Ship extends Placable {
 		this.load = maxLoad;
 	}
 	
-//TODO
+	/**
+	 * Executes a cycle for this ship
+	 */
+	public void step() {
+		//TODO
+	}
+	
+	/**
+	 * Clears all registers (to unset / false)
+	 */
+	public void clearRegisters() {
+		//TODO
+	}
+	
+	/**
+	 * Get the value of a certain register.
+	 * @param register the register to get the value from
+	 * @return the value of sense and ship state registers
+	 */
+	public int getRegister(Register register) {
+		//TODO
+		return 0;
+	}
+	
+	/**
+	 * Sets a certain register
+	 * @param register the register to set
+	 * @param value the value to set
+	 */
+	public void setRegister(Register register, int value) {
+		//TODO
+	}
+	
+	/**
+	 * Resets the boredom of this ship
+	 */
+	public void resetBoredom() {
+		//TODO
+	}
+	
+	/**
+	 * Increases the boredom of this ship by one
+	 */
+	public void increaseBoredom() {
+		//TODO
+	}
+	
+	/**
+	 * Increases the program counter of this ship by one
+	 */
+	public void increasePC() {
+		//TODO
+	}
+
+	
+	public int getLoad() {
+		return load;
+	}
+
+	public void setLoad(int load) {
+		this.load = load;
+	}
+
+	public int getMorale() {
+		return morale;
+	}
+
+	public void setMorale(int morale) {
+		this.morale = morale;
+	}
+
+	public int getPC() {
+		return pc;
+	}
+
+	public void setPC(int pc) {
+		this.pc = pc;
+	}
+
+	public int getRestTime() {
+		return restTime;
+	}
+
+	public void setRestTime(int restTime) {
+		this.restTime = restTime;
+	}
+
+	public Heading getHeading() {
+		return heading;
+	}
+
+	public void setHeading(Heading heading) {
+		this.heading = heading;
+	}
+
+	public int getCondition() {
+		return condition;
+	}
+
+	public void setCondition(int condition) {
+		this.condition = condition;
+	}
+
+	public Faction getFaction() {
+		return faction;
+	}
+
+	public static int getMaxload() {
+		return maxLoad;
+	}
+
+	public static int getMaxmorale() {
+		return maxMorale;
+	}
+
+	public static int getMaxboredom() {
+		return maxBoredom;
+	}
+
+	public int getBoredom() {
+		return boredom;
+	}
 
 }
