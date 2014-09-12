d73c21aac43cd3af52e7ec8d97b3aadae07c85b8
Kerstin <s9kelenh@stud.uni-saarland.de>
2014-09-12 14:37:05 +0200

    Added empty methods to the EntityFactory and added some information to the two parsers

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/EntityFactory.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/EntityFactory.java
@@ -1,5 +1,59 @@
 package de.unisaarland.cs.st.pirates.group1.main.sim.gamestuff;
 
+/**
+ * 
+ * @author Kerstin
+ * @version Version 1.0
+ *
+ */
 public class EntityFactory {
 
+	private int shipNextId;
+	private int treasureNextId;
+	private int buoyNextId;
+	
+	/**
+	 * This method creates a ship using its faction and tile.
+	 * 
+	 * @param faction 
+	 * @param tile 
+	 * @return
+	 */
+	public Ship createShip(Faction faction, Tile tile){
+		//TODO: implement this
+	}
+	
+	/**
+	 * This method creates a buoy using its type (there are six types available), its faction 
+	 * and the tile it is currently placed on.
+	 * 
+	 * @param type
+	 * @param faction
+	 * @param tile
+	 * @return
+	 */
+	public Buoy createBuoy(int type, Faction faction, Tile tile){
+		//TODO: implement this
+	}
+	
+	/**
+	 * This method creates a treasure using its value and the tile it is currently placed on.
+	 * 
+	 * @param value
+	 * @param tile
+	 * @return
+	 */
+	public Treasure createTreasure(int value, Tile tile){
+		//TODO: implement this
+	}
+	
+	/**
+	 * This method creates a kraken using the tile it is currently placed on.
+	 * @param tile
+	 * @return
+	 */
+	public Kraken releaseTheKraken(Tile tile){
+		//TODO: implement this
+	}
+	
 }
