1b34225a300f9c28b87e0362ce3f4d26242b3a49
Thomas Haslbauer <s9thhasl@stud.uni-saarland.de>
2014-09-13 21:16:39 +0200

    Added hashCode() and equals() methods to Faction

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/Faction.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/gamestuff/Faction.java
@@ -1,5 +1,7 @@
 package de.unisaarland.cs.st.pirates.group1.sim.gamestuff;
 
+import java.lang.reflect.Array;
+
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 
 /**
@@ -43,6 +45,10 @@ public class Faction {
 		return name;
 	}
 
+	public Instruction[] getTactics(){
+		return this.tactics.clone();
+	}
+	
 	public void setTactics(Instruction[] tactics) {
 		this.tactics = tactics;
 	}
@@ -88,4 +94,30 @@ public class Faction {
 		//TODO implement
 		return null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Faction other = (Faction) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
 }
