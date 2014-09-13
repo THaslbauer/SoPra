2faf2e9cb04173d76c7d36c315827027a2aa49eb
Christopher Hahn <s9chhahn@stud.uni-saarland.de>
2014-09-13 23:06:10 +0200

    fixing issue#4  ExtendedLogWriter 1.0 -> 1.1

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logger/ExtendedLogWriter.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logger/ExtendedLogWriter.java
@@ -10,14 +10,26 @@
 
 package de.unisaarland.cs.st.pirates.group1.sim.logger;
 
+import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Kraken;
+import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Ship;
+
 public interface ExtendedLogWriter extends LogWriter{
 	
 /**
- * The additional fighting Method, messaging the changes if a fight appears.
- * @param
+ * The additional fighting Method, messaging the changes if a fight between two ships appears.
+ * @param ship the first of the two ships, which is actually stepping
+ * @param otherShip the other ship
  * @return
  * 
  */
-public void fight();
+public void fight(Ship ship, Ship otherShip);
+
+/**
+ * the additinal fightin Method, messaging the changes if a fight between a ship and a kraken appears.
+ * @param ship the ship which is actually stepping
+ * @param kraken the kraken
+ */
+
+public void fight(Ship ship, Kraken kraken);
 
 }
