43a3bc8260bae5deca8c80fd4628b1dfeb67ef86
Christopher Hahn <s9chhahn@stud.uni-saarland.de>
2014-09-15 18:59:32 +0200

    Tests for GoTo and DropInstructions, and Testclasses for gui added

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group1/tests/sim/logic/InstructionTest.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group1/tests/sim/logic/InstructionTest.java
@@ -20,9 +20,14 @@ import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Ship;
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Tile;
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Worldmap;
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Worldmap6T;
+import de.unisaarland.cs.st.pirates.group1.sim.logger.ExtendedLogWriter;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.ElseInstruction;
+import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.normalInstructions.DropInstruction;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.normalInstructions.GotoInstruction;
+import de.unisaarland.cs.st.pirates.group1.tests.sim.logger.TestGui;
+import de.unisaarland.cs.st.pirates.group1.tests.sim.logger.TestGuiDropInstr;
+import de.unisaarland.cs.st.pirates.group1.tests.sim.logger.TestGuiNotify;
 
 public class InstructionTest {
 	
@@ -58,14 +63,79 @@ public class InstructionTest {
 	
 	/**
 	 * testing if the pc after the goto is increased of the right amount
+	 * and also checks if a null logger is handled correctly
 	 */
 	@Test
-	public static void ElseCorrectJumpTest(){
+	public static void GoToCorrectJumpTest(){
 		Instruction goToInstruction = new GotoInstruction(null,1);
 		
 		int pc = ship.getPC();
-		goToInstruction.execute(ship);
+		try{
+		goToInstruction.execute(ship);}
+		catch (NullPointerException e){
+			fail("NullPointerException, because logger is null");		
+		}
 		assertTrue(ship.getPC() == pc + 1);
+	}
+	
+	/**
+	 * testing if the jump to 0 is valid
+	 */
+	
+	@Test
+	public static void GoToCorrectZeroJumpTest(){
+		Instruction goToInstruction = new GotoInstruction(null,0);
+		
+		//int pc = ship.getPC();
+		try{
+		goToInstruction.execute(ship);}
+		catch (NullPointerException e){
+			fail("NullPointerException because logger is null");		
+		}
+		assertTrue( 0 == ship.getPC());
+	}
+	
+	/**
+	 * checks if the logger/gui opens the notify method after receiving this
+	 */
+	
+	@Test
+	public static void GoToLoggerReceives(){
+		TestGui testGui = new TestGuiNotify();
+		Instruction goToInstruction = new GotoInstruction(testGui,12);
+		
+		goToInstruction.execute(ship);
+		assertTrue(testGui.value == -1);
+	}
+	
+	//DropInstruction Tests
+	/**
+	 * checks if notify and create method in gui is called
+	 */
+	@Test
+	public static void ShipDropsLoadTest(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		Instruction dropInstruction = new DropInstruction(testGui);
+		
+		ship.setLoad(4);
+		//checks if create and notify is called
+		dropInstruction.execute(ship);
+		assertTrue(testGui.value == -1);
+		assertTrue(testGui.val == 42);
+	}
+	
+	/**
+	 * checks if PC is increased after Drop
+	 */
+	@Test
+	public static void DropInstructionIncreasePC(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		Instruction dropInstruction = new DropInstruction(testGui);
+		
+		int pc = ship.getPC();
+		
+		dropInstruction.execute(ship);
+		assertTrue(pc == ship.getPC()+1);
 	}
 	
 /*
