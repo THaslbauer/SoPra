b881c41e6052bbf2aa48fb51c0fbdcfd27f8cd46
Christopher Hahn <s9chhahn@stud.uni-saarland.de>
2014-09-17 00:50:32 +0200

    added ifInstruction tests and RepairInstruction Tests

--- simulator/src/test/java/de/unisaarland/cs/st/pirates/group1/tests/sim/logic/ElseInstructionTest.java
+++ simulator/src/test/java/de/unisaarland/cs/st/pirates/group1/tests/sim/logic/ElseInstructionTest.java
@@ -24,6 +24,9 @@ import de.unisaarland.cs.st.pirates.group1.sim.logic.expression.Literal;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.ElseInstruction;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.IfAllInstruction;
+import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.IfAnyInstruction;
+import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.IfInstruction;
+import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructions.RepairInstruction;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.normalInstructions.DropInstruction;
 import de.unisaarland.cs.st.pirates.group1.tests.testUtil.TestGuiDropInstr;
 
@@ -87,18 +90,110 @@ public class ElseInstructionTest {
 		
 		assertTrue(ship.getPC() == 12);
 	}
+	@Test
+	public void ifAllInstructionGetterTest(){
+		Expression[] expArray = new Expression[2];
+		expArray[0] = new Literal(42);
+		expArray[1] = new Literal(1);
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		IfAllInstruction ifAllInstruction = new IfAllInstruction(testGui, 12, expArray);
+		
+		assertTrue(ifAllInstruction.getConditions().equals(expArray));
+	}
 	
+	//IF ANY INSTRUCTION
 	@Test
 	public void ifAnyInstructionTest(){
 		Expression[] expArray = new Expression[2];
 		expArray[0] = new Literal(42);
 		expArray[1] = new Literal(1);
 		TestGuiDropInstr testGui = new TestGuiDropInstr();
-		Instruction ifAllInstruction = new IfAllInstruction(testGui, 12, expArray);
+		Instruction ifAnyInstruction = new IfAnyInstruction(testGui, 12, expArray);
 		
 		//TODO check if this test is right!!
-		ifAllInstruction.execute(ship);
+		ifAnyInstruction.execute(ship);
 		
 		assertTrue(ship.getPC() == 1);
 	}
+	
+	@Test
+	public void ifAnyInstructionGetterTest(){
+		Expression[] expArray = new Expression[2];
+		expArray[0] = new Literal(42);
+		expArray[1] = new Literal(1);
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		IfAnyInstruction ifAnyInstruction = new IfAnyInstruction(testGui, 12, expArray);
+		
+		assertTrue(ifAnyInstruction.getConditions().equals(expArray));
+	}
+	
+	//IF Instruction
+	@Test
+	public void ifInstructionTest(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		Instruction ifInstruction = new IfInstruction(testGui, 12, new Literal(1));
+		
+		//TODO check if this test is right!!
+		ifInstruction.execute(ship);
+		
+		assertTrue(ship.getPC() == 1);
+	}
+	
+	@Test
+	public void ifInstructionNegTest(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		Instruction ifInstruction = new IfInstruction(testGui, 12, new Literal(0));
+		
+		//TODO check if this test is right!!
+		ifInstruction.execute(ship);
+		
+		assertTrue(ship.getPC() == 12);
+	}
+	
+	@Test
+	public void ifInstructionGetterTest(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		Expression test = new Literal(0);
+		IfInstruction ifInstruction = new IfInstruction(testGui, 12, test);
+		
+		assertTrue(ifInstruction.getCondition().equals(test));
+	}
+	//REPAIRINSTRUCTION TEST
+	@Test
+	public void RepairFailTest(){
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		RepairInstruction repInstr = new RepairInstruction(testGui, 12);
+		
+		ship.setCondition(1);
+		repInstr.execute(ship);
+		
+		assertTrue(repInstr.getElsePC() == 12);
+		assertTrue(ship.getCondition() == 1);
+	}
+	
+	@Test
+	public void RepairValidTest(){
+		//build a testmap with only bases
+		Worldmap6T worldTest = new Worldmap6T(2,2,null,null);
+		
+		Position position1 = new Position(0,0);
+		Position position2 = new Position(1,0);
+		Position position3 = new Position(0,1);
+		Position position4 = new Position(1,1);
+		
+		Tile baseTile1 = worldMap.createBaseTile(position1,faction);
+		Tile baseTile2 = worldMap.createBaseTile(position2,faction);
+		Tile baseTile3 = worldMap.createBaseTile(position3,faction);
+		Tile baseTile4 = worldMap.createBaseTile(position3,faction);
+		
+		
+		TestGuiDropInstr testGui = new TestGuiDropInstr();
+		RepairInstruction repInstr = new RepairInstruction(testGui, 12);
+		
+		ship.setCondition(1);
+		repInstr.execute(ship);
+		
+		assertTrue(ship.getCondition() == 3);
+	}
+	
 }
