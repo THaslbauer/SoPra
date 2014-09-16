ca9b90f0cb7c9e5644e18254a21f2c5140667b14
Jens Ulrich Kreber <s9jekreb@stud.uni-saarland.de>
2014-09-17 00:15:22 +0200

    re-fixed main to 3 lines

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/main/Main.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/main/Main.java
@@ -1,10 +1,8 @@
 package de.unisaarland.cs.st.pirates.group1.main;
 
-public class Main {	
-	
-	public static void main(String[] args) { 
-		System.out.println("Hallo Welt!");	
-	} 
-	
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+	}
 }
 
