f4436572709118bb846c3e905d417c0ccd7fe8cb
Christopher Hahn <s9chhahn@stud.uni-saarland.de>
2014-09-18 11:39:58 +0200

    implemented initializeSimulator in Controller

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/driver/Controller.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/driver/Controller.java
@@ -8,8 +8,15 @@
 package de.unisaarland.cs.st.pirates.group1.sim.driver;
 
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
+import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Faction;
+import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Worldmap;
+import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 import de.unisaarland.cs.st.pirates.group1.sim.parser.MapParser;
 import de.unisaarland.cs.st.pirates.group1.sim.parser.TacticsParser;
 
@@ -20,6 +27,12 @@ public class Controller {
 	private TacticsParser tacticsParser;
 	private InputStream mapFile;
 	private List<InputStream> tacticsFile;
+	//TODO CHECK IF THIS IS RIGHT
+	//the seed given from the main/command line
+	private int seed;
+	//the random object
+	private Random random;
+	private OutputStream output;
 
 	/**
 	 * the Constructor for the Controller
@@ -29,14 +42,19 @@ public class Controller {
 	 * @param tacticsParser the Tactic parser which the controller starts
 	 * @param mapFile the mapfile, for parsing the map
 	 * @param tacticsFile the tacticsfile for parsing the tactics
+	 * @param seed the integer from the commandline giving us a valid seed
+	 * @param output the Output Steam, which is needed so the Controller can initialize the InfoPoint
 	 */
 public Controller(Simulator simulator, MapParser mapParser,
-		TacticsParser tacticsParser, InputStream mapFile, List<InputStream> tacticsFile){
+		TacticsParser tacticsParser, InputStream mapFile, List<InputStream> tacticsFile,int seed, OutputStream output){
 	this.simulator = simulator;
 	this.mapParser = mapParser;
 	this.tacticsParser = tacticsParser;
 	this.mapFile = mapFile;
 	this.tacticsFile = tacticsFile;
+	this.seed = seed;
+	this.random = new Random(seed);
+	this.output = output;
 	
 }
 	
@@ -45,6 +63,29 @@ public Controller(Simulator simulator, MapParser mapParser,
 	 * 
 	 * Setter and Getter
 	 */
+public int getSeed() {
+	return seed;
+}
+
+public void setSeed(int seed) {
+	this.seed = seed;
+}
+
+public Random getRandom() {
+	return random;
+}
+
+public void setRandom(Random random) {
+	this.random = random;
+}
+
+public OutputStream getOutput() {
+	return output;
+}
+
+public void setOutput(OutputStream output) {
+	this.output = output;
+}
 	
 	public Simulator getSimulator() {
 		return simulator;
@@ -86,17 +127,61 @@ public Controller(Simulator simulator, MapParser mapParser,
 		this.tacticsFile = tacticsFile;
 	}
 
-	
 	/**
-	 * Initializes the Simulator.
+	 * converts the inputStream to String, taken from stackoverflow.com
+	 * @param is
+	 * @return the String that represents the map
+	 */
+	
+		private static String convertStreamToString(java.io.InputStream is) {
+		    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+		    return s.hasNext() ? s.next() : "";
+		}
+		
+	/**
+	 * Initializes the Simulator and also the Infopoint
 	 * 
 	 * @param
 	 * @return void
 	 */
 public void initializeSimulator(){
-	/*TODO IMPLEMENT THIS
-	 * 
-	 */
+	
+	List<String> stringList = new LinkedList<String>();
+	for(InputStream in : tacticsFile){
+		stringList.add(convertStreamToString(in));
+	}
+	String[] strings = (String[])stringList.toArray();
+	
+	//initializes the LogWriter
+	simulator.getLogWriter().init(output, convertStreamToString(mapFile) , strings);
+	
+	
+	//sets the randomobject to the simulator
+	simulator.setRandom(random);
+	
+	//parses the map and give it to the simulator
+	mapParser.parseMap(mapFile, simulator);
+	
+	//get the factionarray
+	List<Faction> factions = simulator.getFactions();
+	
+	int factionCounter = 0;
+	//check if there are enough tacticsfiles for the factions
+	if(factions.size() <= tacticsFile.size()){
+	//iterate through the tacticsfile
+	for(InputStream tactic : tacticsFile){
+		Instruction[] instrArray = tacticsParser.parseTactics(tactic, random);
+		try{
+		factions.get(0).setTactics(instrArray);
+		//breaks out from the loop, if there are no factions left to add tactics to
+		}catch(IndexOutOfBoundsException e){
+			break;
+		}
+		factionCounter++;
+	}
+	}else{
+		throw new IllegalArgumentException("There are not enough tactics for the factions");
+	}
 	
 }
 
