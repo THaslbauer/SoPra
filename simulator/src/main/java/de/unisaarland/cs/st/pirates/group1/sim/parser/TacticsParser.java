429d19bb27c05c4443a47d0997f30296ad3fe0d9
Kerstin <s9kelenh@stud.uni-saarland.de>
2014-09-19 23:42:20 +0200

    began to write TacticsParser

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/parser/TacticsParser.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/parser/TacticsParser.java
@@ -2,6 +2,7 @@ package de.unisaarland.cs.st.pirates.group1.sim.parser;
 
 import java.io.InputStream;
 import java.util.Random;
+import java.util.Scanner;
 
 import de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.Instruction;
 
@@ -23,7 +24,88 @@ public class TacticsParser {
 	 * @param random
 	 */
 	public Instruction[] parseTactics(InputStream input, Random random){
-		//TODO: implement this
+		
+		//convert the input to string
+		String tactics_string = this.convertStreamToString(input);
+	
+		//split the input string into an array of instructions
+		String[] tactics_string_array = tactics_string.split("\n");
+		
+		for(int i = 0; i< tactics_string_array.length; i++){
+			
+			//this int is needed to find out how many arguments there are
+			int size;
+			
+			String[] instruction_array = tactics_string_array[i].split(" ");
+			
+			try{
+				int expr_num = instruction_array.length;
+				
+				String name = instruction_array[0];
+				
+				switch(name){
+				
+				case "turn":
+					break;
+					
+				case "mark":
+					break;
+					
+				case "unmark":
+					break;
+					
+				case "move":
+					break;
+					
+				case "pickup":
+					break;
+					
+				case "drop":
+					break;
+					
+				case "flipzero":
+					break;
+					
+				case "goto":
+					break;
+					
+				case "sense":
+					break;
+					
+				case "if":
+					break;
+				
+				case "ifall":
+					break;
+				
+				case "ifany":
+					break;
+					
+				case "refresh":
+					break;
+					
+				default:
+					throw new IllegalArgumentException("This instruction does not exist");
+				}
+			}
+			
+			//TODO: find out which exceptions are necessary
+			catch(Exception e){
+				
+			}
+			
+			
+			
+		}
+		
+		//TODO: delete this
 		return null;
+		
+	}
+	
+	private String convertStreamToString(InputStream input){
+		try(Scanner scan = new Scanner(input).useDelimiter("\\A")){
+			return scan.hasNext() ? scan.next() : "";
+		}
 	}
 }
