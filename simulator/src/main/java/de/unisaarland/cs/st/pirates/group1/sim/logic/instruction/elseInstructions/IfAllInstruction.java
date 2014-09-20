96ba8e865cf7687252aca58f4354d562e536c5ac
Nico <s9nigers@stud.uni-saarland.de>
2014-09-20 21:03:10 +0200

    if, ifany and ifall done

--- simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/elseInstructions/IfAllInstruction.java
+++ simulator/src/main/java/de/unisaarland/cs/st/pirates/group1/sim/logic/instruction/elseInstructions/IfAllInstruction.java
@@ -2,15 +2,18 @@ package de.unisaarland.cs.st.pirates.group1.sim.logic.instruction.elseInstructio
 
 import de.unisaarland.cs.st.pirates.group1.sim.gamestuff.Ship;
 import de.unisaarland.cs.st.pirates.group1.sim.logger.ExtendedLogWriter;
+import de.unisaarland.cs.st.pirates.group1.sim.logger.LogWriter.Entity;
+import de.unisaarland.cs.st.pirates.group1.sim.logger.LogWriter.Key;
 import de.unisaarland.cs.st.pirates.group1.sim.logic.expression.Expression;
 
 /**
  * Represents an if-statement where all conditions are linked together with AND.
- * @author thomas
+ * @author Nico
  *
  */
-public class IfAllInstruction extends ElseInstruction {
-	Expression[] conditions;
+public class IfAllInstruction extends ElseInstruction
+{
+	private Expression[] conditions;
 
 	/**
 	 * Creates the instruction
@@ -28,9 +31,28 @@ public class IfAllInstruction extends ElseInstruction {
 	}
 
 	@Override
-	public void execute(Ship ship) {
-		// TODO Auto-generated method stub
-
+	public void execute(Ship ship)
+	{
+		int value = 1;
+		
+		for(Expression condition : conditions)
+		{
+			value = value & condition.evaluate(ship.getRegisters());
+		}
+		
+		if(value == 1)
+		{
+			ship.increasePC();
+			
+			this.logger.notify(Entity.SHIP, ship.getId(), Key.PC, ship.getPC());
+			
+			this.cycle(ship);
+			return;
+		}
+		
+		this.elseJump(ship);
+		this.cycle(ship);
+		return;
 	}
 
 }
