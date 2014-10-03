5e2e205b88e7783b454dfe53c849786f86e31b53
Thomas Haslbauer <s9thhasl@stud.uni-saarland.de>
2014-10-03 15:56:59 +0200

    Added the first few classes and some fxml files for creating our UI

---
+++ gui/src/main/java/util/MessageBox.java
@@ -0,0 +1,53 @@
+package util;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MessageBox extends VBox {
+	
+	private Stage ownStage;
+	
+	public MessageBox (Stage ownStage, String title, String messageText) {
+		this.ownStage = ownStage;
+		this.ownStage.setTitle(title);
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/util/MessageBox.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+		try {
+		loader.load();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			throw new UnsupportedOperationException(e.getCause());
+		}
+		this.messageText.setText(messageText);
+	}
+
+	@FXML
+    private Button OKButton;
+
+    @FXML
+    private Text messageText;
+
+    @FXML
+    void onOKButtonClick(ActionEvent event) {
+    	ownStage.close();
+    }		
+    
+    public static void displayMessage(String title, String text) {
+    	System.out.println(text);
+    	Stage error = new Stage();
+    	MessageBox errorM = new MessageBox(error, "Cycle count error", "Cycle count has to be number");
+    	Scene errorS = new Scene(errorM);
+    	error.setScene(errorS);
+    	error.showAndWait();
+    }
+}
